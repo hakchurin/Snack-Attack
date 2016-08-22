869a06826600d3b1a67ecabd89d311f635344f08
Haley Akchurin <hdakchurin@gmail.com>
2016-08-22 09:34:35 -0500

    slider

--- app/scripts/game.js
+++ app/scripts/game.js
@@ -1,4 +1,4 @@
-import Backbone from 'backbone';
+import Backbone from 'backbone'; //maybe no
 import $ from 'jquery';
 import router from './router';
 import endGameModal from './endGameModal';
@@ -10,27 +10,26 @@ function gameView() {
 
     var canvas = document.getElementById("screen");
     var ctx = canvas.getContext("2d");
-    var spawnLineY = 25;
+    var spawnLineY = 25; //what is this
     var spawnRate = 1500;
     var spawnRateOfDescent = 1;
     var lastSpawn = -1;
-    var objects = [];
+    // var objects = [];
+    // var player = [];
+
     var startTime = Date.now();
-    var gameSize = {x: canvas.width,y: canvas.height};
+    var gameSize = {
+        x: canvas.width,
+        y: canvas.height
+    };
     var score = 0;
     var lives = 3;
     var input = 60;
     var paused = true;
-    var game;
+    var game; //?
     var countdown;
     canvas.width = $(window).width();
     canvas.height = $(window).height() - 100;
-
-
-
-
-
-
 
 
     function calculateTime(timer) {
@@ -40,12 +39,13 @@ function gameView() {
         var time = (mins < 10 ? "0" : "") + mins + ":" + (secs < 10 ? "0" : "") + secs;
         return time;
     };
+
     var startInterval = function() {
         $("#timer").text(calculateTime(input));
         input--;
         countdown = setInterval(function() {
             var data = calculateTime(input)
-            if (input && lives > 0) {
+            if (input > 0 && lives > 0) {
                 $("#timer").text(data);
                 input--;
             } else {
@@ -55,66 +55,136 @@ function gameView() {
         }, 1000);
     }
 
+    var Keyboarder = function() {
+        var keyState = {};
+        window.onkeydown = function(e) {
+            keyState[e.keyCode] = true;
+        };
+        window.onkeyup = function(e) {
+            keyState[e.keyCode] = false;
+        };
+        this.isDown = function(keyCode) {
+            return keyState[keyCode] === true;
+        };
+        this.KEYS = {LEFT: 37,RIGHT: 39};
+    }
+
+    var gameSize = {x: canvas.width, y: canvas.height};
+    function Player(game, gameSize) {
+
+        this.game = game;
+        this.size = {x: 100, y: 20};
+        this.center = {x: gameSize.x, y: gameSize.y - this.size.x};
+        this.keyboarder = new Keyboarder();
+
+        this.update = function() {
+            if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT)) {
+                this.center.x = Math.max(25, this.center.x - 15);
+                this.draw(gameSize)
+            } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
+                this.center.x = Math.min($(window).width() - 25, this.center.x + 15);
+                this.draw(gameSize)
+            }
+        }
+        this.drawRect = function() {
+          ctx.fillRect(this.center.x - this.size.x / 2,
+              this.center.y - this.size.y / 2,
+              this.size.x, this.size.y);
+      }
+      this.draw = function(gameSize) {
+          ctx.clearRect(0, 0, gameSize.x, gameSize.y);
+          this.drawRect()
+
+      }
+    };
+
+  var gameSize = {x: canvas.width, y: canvas.height};
+  let player = new Player(ctx,gameSize)
+
+//
+//   function SpawnRandomObject() {
+//       this.size = {x: 15,y: 0};
+//       this.x = Math.random() * (canvas.width - 30) + 15;
+//       this.objects = {x: x,y: spawnLineY,
+//           center: {x: x + 7.5,y: 50 - size.x},
+//           size: {x: 15,y: 0},
+//       }
+//       objects.push(object);
+//   }
+//
+// this.update = function(object){
+//   for (var i = 0; i < this.objects.length; i++) {
+//       this.obj = objects[i];
+//       var size = {x: 15,y: 0};
+//       obj.y += spawnRateOfDescent;
+//       ctx.beginPath();
+//       ctx.arc(obj.x, obj.y, size.x, size.y, Math.PI * 2);
+//       ctx.fill();
+//   }
+//
+//
+// }
+//
+//
+//   let spawnRandomObject = new SpawnRandomObject(gameSize);
+
+
+
+
+
+
 
 
 
     function Game() {
-        var gameSize = {x: canvas.width,y: canvas.height};
-        this.bodies = objects.concat(new Player(this, gameSize));
+      console.log('running game function');
+        var gameSize = {x: canvas.width, y: canvas.height};
 
+        // this.bodies = objects.concat(new Player(this, gameSize));
+        // this.player= [new Player(this, gameSize)];
+        this.player = player
+        // this.fallingObj = object
 
+        var self = this;
         function StartOfGame() {
             $("#screen").hide();
             $(".StartScreen").show();
+            $('#startBtn').on('click', function() {
+                $(".StartScreen").hide();
+                $("#screen").show();
+                startInterval()
+                paused = false;
+                requestAnimationFrame(tick);
+            })
+            self.player.drawRect()
         }
         StartOfGame();
-        $('#startBtn').on('click', function() {
-            $(".StartScreen").hide();
-            $("#screen").show();
-            startInterval()
-            paused = false;
-            requestAnimationFrame(tick);
-        })
-        var self = this;
-        var tick = function() {
+
+
+        var tick = function() { //syntax consitant
+          // console.log('in tick');
+          self.player.update()
             self.update();
-            self.draw(screen, gameSize);
             if (paused) {
                 cancelAnimationFrame(tick);
             }
 
-            function spawnRandomObject() {
-                var size = {x: 15,y: 0};
-                var x = Math.random() * (canvas.width - 30) + 15;
-                var object = {x: x, y: spawnLineY,
-                    center: {x: x + 7.5, y: 50 - size.x},
-                    size: {x: 15, y: 0},
-                }
-                objects.push(object);
-            }
+
+
             var time = Date.now();
             if (time > (lastSpawn + spawnRate)) {
                 lastSpawn = time;
-                spawnRandomObject();
+                // spawnRandomObject();
             }
-            for (var i = 0; i < objects.length; i++) {
-                var object = objects[i];
-                var size = {x: 15, y: 0};
-                object.y += spawnRateOfDescent;
-                ctx.beginPath();
-                ctx.arc(object.x, object.y, size.x, size.y, Math.PI * 2);
-                ctx.fill();
-            }
-
 
             function endGame() {
                 $("#screen").hide();
-                $("#score").text(score);
+                $("#score").text("Score: " + score);
                 $(".FinishScreen").show();
                 ctx.clearRect(0, 0, canvas.width, canvas.height);
                 self.update();
             }
-              if (lives === 0 || input === 0) {
+            if (lives === 0 || input === 0) {
                 cancelAnimationFrame(tick);
                 endGame();
             }
@@ -125,11 +195,11 @@ function gameView() {
         tick();
 
         $('#restartBtn').on('click', function() {
-          requestAnimationFrame(tick);
-          tick();
-          startInterval();
-          resetView();
-          this.bodies;
+            requestAnimationFrame(tick);
+            tick();
+            startInterval();
+            resetView();
+            this.bodies;
         })
 
         $('#pause').on('click', function() {
@@ -142,6 +212,7 @@ function gameView() {
             requestAnimationFrame(tick);
             startInterval();
         })
+
         function resetView() {
             score = 0;
             lives = 3;
@@ -169,87 +240,36 @@ function gameView() {
                     return false;
                 }
             }
+            //
+            // var filteredObjects = objects.filter((current, i, arr) => {
+            //     if (hitTestPoint(this.bodies[0], current)) {
+            //         score += 100;
+            //         return false;
+            //     } else if (current.y === $(window).height()) {
+            //         lives -= 1;
+            //         return false;
+            //     } else {
+            //         return true
+            //     }
+            // })
+            //
+            // objects = filteredObjects;
 
-            var newObject = objects.filter((current, i, arr) => {
-                if (hitTestPoint(this.bodies[0], current)) {
-                    score += 100;
-                    return false;
-                } else if (current.y === $(window).height()) {
-                    lives -= 1;
-                    return false;
-                } else {
-                    return true
-                }
-            })
-
-            objects = newObject;
-            for (var i = 0; i < this.bodies.length; i++) {
-                this.bodies[i].update();
-            }
+        //     for (var i = 0; i < this.bodies.length; i++) {
+        //         this.bodies[i].update();
+        //     }
         },
-        draw: function(screen, gameSize) {
-            ctx.clearRect(0, 0, gameSize.x, gameSize.y);
-            for (var i = 0; i < this.bodies.length; i++) {
-                drawRect(screen, this.bodies[i]);
-            }
-        }
-    };
-
-    var Player = function(game, gameSize) {
-        this.game = game;
-        this.size = {x: 100,y: 20};
-        this.center = {x: gameSize.x, y: gameSize.y - this.size.x};
-        this.keyboarder = new Keyboarder();
-    };
-
-    Player.prototype = {
-        update: function() {
-            if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT)) {
-                this.center.x = Math.max(25, this.center.x - 15);
-            } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
-                this.center.x = Math.min($(window).width() - 25, this.center.x + 15);
-            }
-        }
-    };
-
-    var drawRect = function(screen, body) {
-        ctx.fillRect(body.center.x - body.size.x / 2,
-            body.center.y - body.size.y / 2,
-            body.size.x, body.size.y);
-    };
-
-
-    var Keyboarder = function() {
-        var keyState = {};
-        window.onkeydown = function(e) {
-            keyState[e.keyCode] = true;
-        };
-        window.onkeyup = function(e) {
-            keyState[e.keyCode] = false;
-        };
-        this.isDown = function(keyCode) {
-            return keyState[keyCode] === true;
-        };
-        this.KEYS = {
-            LEFT: 37,
-            RIGHT: 39,
-            SPACE: 32
-        };
-    }
 
 
 
-    var colliding = function(b1, b2) {
-        return !(b1 === b2 ||
-            b1.center.x + b1.size.x / 2 < b2.center.x - b2.size.x / 2 ||
-            b1.center.y + b1.size.y / 2 < b2.center.y - b2.size.y / 2 ||
-            b1.center.x - b1.size.x / 2 > b2.center.x + b2.size.x / 2 ||
-            b1.center.y - b1.size.y / 2 > b2.center.y + b2.size.y / 2);
+
     };
 
 
 
-    $(document).ready(function() {});
+
+
+    // $(document).ready(function() {});
     // window.setInterval(function(){
     //   spawnRateOfDescent += 0.5
     //   spawnRate -= spawnRate / 100 * 20;
